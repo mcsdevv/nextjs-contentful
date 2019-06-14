59e2a47ebf5a883e334a940e9012853598f1c1d2
msweeneydev <mail@msweeneydev.com>
2019-06-14 21:42:48 +0100

    change to .env.build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-const config = require('./.contentful.json');
-
 module.exports = {
   target: 'serverless',
   env: {
-    SPACE_ID: config.SPACE_ID,
-    ACCESS_TOKEN: config.ACCESS_TOKEN
+    SPACE_ID: process.env.SPACE_ID,
+    ACCESS_TOKEN: process.env.ACCESS_TOKEN
   }
 };
