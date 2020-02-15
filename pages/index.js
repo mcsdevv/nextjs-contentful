a477fcedf011c8a147ddddef13566e1f1cd6b04d
msweeneydev <mail@msweeneydev.com>
2020-02-15 23:45:20 +0000

    fix due to contentful breaking api change

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,24 @@
-import { useEffect, useState } from 'react';
-import Head from 'next/head';
-import Post from '../components/post';
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import Post from "../components/post";
 
-const client = require('contentful').createClient({
+const client = require("contentful").createClient({
   space: process.env.SPACE_ID,
   accessToken: process.env.ACCESS_TOKEN
 });
 
 function HomePage() {
-  async function fetchContentTypes() {
-    const types = await client.getContentTypes();
-    if (types.items) return types.items;
-    console.log('Error getting Content Types.');
-  }
-  async function fetchEntriesForContentType(contentType) {
-    const entries = await client.getEntries({
-      content_type: contentType.sys.id
-    });
+  async function fetchEntries() {
+    const entries = await client.getEntries();
     if (entries.items) return entries.items;
     console.log(`Error getting Entries for ${contentType.name}.`);
   }
+
   const [posts, setPosts] = useState([]);
+
   useEffect(() => {
     async function getPosts() {
-      const contentTypes = await fetchContentTypes();
-      const allPosts = await fetchEntriesForContentType(contentTypes[0]);
+      const allPosts = await fetchEntries();
       setPosts([...allPosts]);
     }
     getPosts();
